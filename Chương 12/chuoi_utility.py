d83fc5098cb80d0d7d7240d7e6094f7086508be9
Nguyen Thi Xuan <ntxuan2517400072@sv.uneti.edu.vn>
2025-12-26 22:43:51 +0700

    Chuong 12

---
+++ Chương 12/chuoi_utility.py
@@ -0,0 +1,4 @@
+def dao_nguoc_chuoi(chuoi):
+    return chuoi[::-1]
+def dem_so_tu(chuoi):
+    return len(chuoi.split())
---
+++ Chương 12/su_dung.py
@@ -0,0 +1,6 @@
+from may_tinh.nang_cao import luy_thua, can_bac_hai
+
+print("Cộng:", cong(3, 2))
+print("Trừ:", tru(5, 1))
+print("Lũy thừa:", luy_thua(2, 3))
+print("Căn bậc hai:", can_bac_hai(16))
