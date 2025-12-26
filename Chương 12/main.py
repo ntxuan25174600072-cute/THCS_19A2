4b0dacc423fc443403ab5aadb08667e7012d533a
Nguyen Thi Xuan <ntxuan2517400072@sv.uneti.edu.vn>
2025-12-26 22:51:09 +0700

    Chuong 12

---
+++ Chương 12/main.py
@@ -0,0 +1,10 @@
+#Bài 1
+from hinh_hoc import chu_vi_hinh_vuong, chu_vi_hinh_tron
+print(chu_vi_hinh_vuong(5))
+print(chu_vi_hinh_tron(3))
+
+#Bài 2
+from chuoi_utility import dao_nguoc_chuoi
+chuoi = "Hoc Python that de"
+print("Chuỗi đảo:", dao_nguoc_chuoi(chuoi))
+
